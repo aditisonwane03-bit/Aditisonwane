a9fe99d99b192eb012779cb7291531183c9044a2
aditisonwane03-bit <aditisonwane03@gmail.com>
2026-09-12 20:13:59 +0530

    Create expt10.c

---
+++ expt10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+int n, i;
+printf("Enter a number: ");
+scanf ("%d", &n);
+// Using while 1o0p
+printf("\nMultiplication Table using while loop: \n");
+1 = 1;
+while (i <= 10) {
+printf("%d × %d = %d\n", n, i, n * i);
+i+t;
+// Using do-while lo0p
+printf("\nMultiplication Table using do-while loop: \n");
+1 = 1; do {
+printf("%d x %d = %d\n", n, i, n * i);
+i+t;
+} while (i <= 10);
+// Using for loop
+printf("\nMultiplication Table using for loop:\n");
+for (i = 1; i <= 10; it+) {
+printf("%d x %d = %d\n", n,
+i, n * i);
+return 0;
+}
